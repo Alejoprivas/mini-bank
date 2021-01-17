b78863d3379a3b2baaebcea5f3cc9f5d72d9bfc3
Alejandro <alperideveloper@gmail.com>
2021-01-16 23:56:13 -0300

    added component for accounts

--- client/src/app/core/components/balance/balance.component.ts
+++ client/src/app/core/components/balance/balance.component.ts
@@ -1,20 +1,28 @@
-import { RepositionScrollStrategy } from '@angular/cdk/overlay';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { AuthenticationService } from '../../_auth/authentication.service';
-
+import { AccountsService } from '../../services/accounts.service'
 @Component({
   selector: 'app-balance',
   templateUrl: './balance.component.html',
   styleUrls: ['./balance.component.sass']
 })
 export class BalanceComponent implements OnInit {
-  balance;
-  constructor(private authenticationService: AuthenticationService) 
-                { 
-                  this.authenticationService.getUser().subscribe(Response=>{
-                    this.balance = Response.balance;
-                  })
-                }
+  cuentas;
+  selectedCuenta;
+  @Output() public selectCuentaEvent = new EventEmitter<any>();
+
+  constructor(
+    private accountService: AccountsService
+  ) {
+    this.accountService.getAccounts().subscribe(Accounts => {
+      this.cuentas = Accounts;
+      this.selectedCuenta = Accounts[0];
+    });
+  }
+  public selectAccount(index) {
+    this.selectedCuenta = this.cuentas[index];
+    this.selectCuentaEvent.emit(this.selectedCuenta);
+  }
 
   ngOnInit() {
   }
