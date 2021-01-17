d52abc54f0ee6eb3353eb714b17d06e298a804f8
Alejandro <alperideveloper@gmail.com>
2021-01-16 23:58:20 -0300

    added new service for account management

---
+++ client/src/app/core/services/accounts.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { shareReplay, map, tap } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+  contextUrl = environment.apiUrl;
+  constructor(private http: HttpClient) { }
+
+  getAccounts(): Observable<any> {
+    return this.http.get<any>(this.contextUrl + '/accounts')
+      .pipe(
+        map(account => account)
+      );
+  }
+
+}
